20d73a5c6b4a7b301445e238e0aadfc683c8387f
行事光 <h.gyoji.vrds@gmail.com>
2025-02-17 15:20:43 +0900

    add web components

--- smart_box/demo/js/main.js
+++ smart_box/demo/js/main.js
@@ -1,24 +1,7 @@
-import { smartBox } from '../../smart_box.js';
-
-// API（もしかして検索）あり、キーワード選択欄なし、最大表示数指定：5
-smartBox('NANDO', './tsv/NANDO_sample.tsv', {
-  api_url: 'http://localhost:5555/moshikashite_test_api?text=',
-  max_results: 5,
-});
-
-// API（もしかして検索）あり、キーワード選択欄あり
-smartBox('MONDO', './tsv/mondo_sample.tsv', {
-  api_url: 'http://localhost:5555/moshikashite_test_api?text=',
-  include_no_match: true,
-});
-
-// API（もしかして検索）なし、キーワード選択欄なし
-smartBox('ICD10', './tsv/icd10_sample.tsv');
-
-document.addEventListener('selectedLabel', function (event) {
-  const inputBoxId = event.detail.inputBoxId;
+document.addEventListener('selectedSmartBoxLabel', function (event) {
+  const smartBoxId = event.detail.smartBoxId;
   const labelInfo = event.detail.labelInfo;
-  switch (inputBoxId) {
+  switch (smartBoxId) {
     case 'NANDO':
       console.log('NANDO:', labelInfo);
       break;
@@ -29,7 +12,7 @@ document.addEventListener('selectedLabel', function (event) {
       console.log('ICD10:', labelInfo);
       break;
     default:
-      console.error('Unknown input box ID:', inputBoxId);
+      console.error('Unknown input box ID:', smartBoxId);
   }
 });
 
